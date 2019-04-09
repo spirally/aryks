5052391779b77feae46da9a9771603ddeecb1888
spiridonov <spiridonov@vistgroup.ru>
2019-04-09 22:42:05 +0700

    shop translation

--- shop/models.py
+++ shop/models.py
@@ -5,7 +5,7 @@ from django.urls import reverse
 
 # Модель категории
 class Category(models.Model):
-    name = models.CharField(max_length=200, db_index=True, verbose_name='Имя')
+    name = models.CharField(max_length=200, db_index=True, verbose_name=_('Имя'))
     slug = models.SlugField(max_length=200, db_index=True, unique=True)
 
     class Meta:
@@ -23,15 +23,15 @@ class Category(models.Model):
 # Модель продукта
 class Product(models.Model):
     category = models.ForeignKey(Category, related_name='products', verbose_name=_("Категория"), on_delete=models.CASCADE)
-    name = models.CharField(max_length=200, db_index=True, verbose_name="Название")
+    name = models.CharField(max_length=200, db_index=True, verbose_name=_("Название"))
     slug = models.SlugField(max_length=200, db_index=True)
-    image = models.ImageField(upload_to='products/%Y/%m/%d/', blank=True, verbose_name="Изображение товара")
-    description = models.TextField(blank=True, verbose_name="Описание")
-    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
-    stock = models.PositiveIntegerField(verbose_name="На складе")
-    available = models.BooleanField(default=True, verbose_name="Доступен")
-    created = models.DateTimeField(auto_now_add=True, verbose_name='Создан')
-    updated = models.DateTimeField(auto_now=True, verbose_name='Обновлен')
+    image = models.ImageField(upload_to='products/%Y/%m/%d/', blank=True, verbose_name=_("Изображение товара"))
+    description = models.TextField(blank=True, verbose_name=_("Описание"))
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_("Цена"))
+    stock = models.PositiveIntegerField(verbose_name=_("На складе"))
+    available = models.BooleanField(default=True, verbose_name=_("Доступен"))
+    created = models.DateTimeField(auto_now_add=True, verbose_name=_('Создан'))
+    updated = models.DateTimeField(auto_now=True, verbose_name=_('Обновлен'))
 
     class Meta:
         ordering = ['name']
